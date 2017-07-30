df1feb180d7235ad5cfe78bb87d9b6eadb82e9d9
DESKTOP-5JUGFHH\Rizwan <rizwanzaheer003@gmail.com>
2017-07-30 20:46:22 +0500

    good bye

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
+  res.send({ hi: 'there', bye: "good bye!"});
 });
 app.listen(PORT, (err) => {
   if(err) return console.log(err);
