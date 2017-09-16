d552bbe40a1fab49d5d6147d57394d18ff107b9a
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-17 02:17:44 +0500

    redux form field added

--- client/src/components/surveys/SurveyForm.jsx
+++ client/src/components/surveys/SurveyForm.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
-
-
+import { reduxForm, Field } from "redux-form";
 
 class SurveyForm extends Component {
   render() {
-    return <div>Survey Form with redux form!</div>;
+    return (
+      <div>
+        Survey Form with redux form!
+        <form onSubmit={this.props.handleSubmit(value => console.log(value))}>
+          <Field
+            placholder="survey"
+          type="text" name="surveyTitle" component="input" />
+          <button type="submit">Submit</button>
+         </form>
+      </div>
+      
+    );
   }
 }
 export default reduxForm({
-  form: 'surveyForm'
+  form: "surveyForm"
 })(SurveyForm);
