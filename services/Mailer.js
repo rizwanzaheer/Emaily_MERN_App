463e12cabe107bcaac82c8039836a2f344f69257
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-22 10:58:56 +0500

    fixing design/ Redirect pages

--- services/Mailer.js
+++ services/Mailer.js
@@ -41,7 +41,7 @@ class Mailer extends helper.Mail {
       path: "/v3/mail/send",
       body: this.toJSON()
     });
-    
+
     const response = await this.sgApi.API(request);
     return response;
   }
