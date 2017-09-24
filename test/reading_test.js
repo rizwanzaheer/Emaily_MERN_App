499f89f6f8b6b0c8373cea9aa6e13fe5553b2a15
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-24 21:26:22 +0500

    findOne record test on the base of particular id

--- test/reading_test.js
+++ test/reading_test.js
@@ -12,9 +12,14 @@ describe("Reading users out of the database", () => {
   it("finds all users with the name of Joe", done => {
     // Joe instance of a User
     User.find({ name: "Joe" }).then(users => {
-      console.log(users[0]._id);
-      console.log(joe._id);
       assert(users[0]._id.toString() === joe._id.toString());
+      done();
+    });
+  });
+
+  it("find a user with a particular id", done => {
+    User.findOne({ _id: joe._id }).then(users => {
+      assert(users.name === "Joe");
       done();
     });
   });
