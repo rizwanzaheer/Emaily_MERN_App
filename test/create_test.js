99b04eff2791f1fdd4d2083b58065d2fc3bd5a1c
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-24 16:16:32 +0500

    adding test script using mocha adding test case and check it

--- test/create_test.js
+++ test/create_test.js
@@ -1,6 +1,8 @@
 // purpose of this file is to crease new user and test it
+const assert = require('assert');
+
 describe("Creating records", () => {
   it('saves a user', ()=>{
-    
+    assert( 1 + 1 === 2);
   });
 });
