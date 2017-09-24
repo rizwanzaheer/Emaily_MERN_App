09bb8c5f7a4a2c04c59d2848d28a602ef9250548
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-24 20:24:08 +0500

    find user from Users adding reading test function to find all users with the specific name

---
+++ test/reading_test.js
@@ -0,0 +1,19 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Reading users out of the database", () => {
+  let joe;
+  beforeEach(done => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => {
+      done();
+    });
+  });
+  it("finds all users with the name of Joe", done => {
+    // Joe instance of a User
+    User.find({ name: "Joe" }).then(users => {
+      console.log(users);
+      done();
+    });
+  });
+});
