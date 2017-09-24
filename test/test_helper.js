5b8bea168302b012266a872ac8d0db40b59d925f
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-24 17:55:33 +0500

    testing user again and again it's create user repeative handle or clear database before running test

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,3 +4,12 @@ mongoose.connect(keys.localMongoURL);
 mongoose.connection
   .once("open", () => console.log("Good to Go!"))
   .on("error", error => console.error("Error :", error));
+
+// this is a hoook
+beforeEach(done => {
+  // delete all record in the users collection
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
+  });
+});
