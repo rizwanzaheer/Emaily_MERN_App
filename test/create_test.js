0d984802521ebc2ae000ba31a7856ed6f0e1d8af
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-24 19:15:01 +0500

    es6 default promise add adding before hook to create one time connection and assert the test

--- test/create_test.js
+++ test/create_test.js
@@ -3,10 +3,16 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Creating records", () => {
-  it("saves a user", () => {
+  it("saves a user", done => {
     const joe = new User({
       name: "joe"
     });
-    joe.save();
+    joe.save().then(() => {
+      // has joe been saved successfully?
+      // in Model isNew  === true
+      // when saved successfully then isNew === false
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 const keys = require("../config//keys");
-mongoose.connect(keys.localMongoURL);
-mongoose.connection
-  .once("open", () => console.log("Good to Go!"))
-  .on("error", error => console.error("Error :", error));
+
+// ES6 Promise global
+mongoose.Promise = global.Promise;
+
+// this is a hoook
+// before only call at one time
+// to create a connection
+// and run all test
+before(done => {
+  mongoose.connect(keys.localMongoURL);
+  mongoose.connection
+    .once("open", () => {
+      console.log("Good to Go!");
+      done();
+    })
+    .on("error", error => console.error("Error :", error));
+});
 
 // this is a hoook
 beforeEach(done => {
