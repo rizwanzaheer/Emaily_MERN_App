28ae9162aec17e7df8df7e507aaa5e5775a614a3
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-25 23:19:27 +0500

    complete CRUD with test

--- test/update_test.js
+++ test/update_test.js
@@ -4,25 +4,44 @@ const User = require("../src/user");
 describe("Updatting records", () => {
   let joe;
   beforeEach(done => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(() => done());
   });
   function assertName(operation, done) {
     operation.then(() => User.find({})).then(users => {
       assert(users.length === 1);
-      assert(users[0].name === "Rizwan");
+      assert(users[0].name === "Alex");
       done();
     });
   }
   it("instance type using set n save", done => {
     console.log(joe);
     // set func is only change in memory not in db
-    joe.set("name", "Rizwan");
+    joe.set("name", "Alex");
     assertName(joe.save(), done);
     console.log(joe);
   });
   it("A model instance can update!", done => {
     joe.update({ name: "Alex" });
+    done();
+  });
+  it("A model class can update", done => {
+    // find name 'Joe' and update with "Alex"
+    assertName(User.update({ name: "Joe" }, { name: "Alex" }), done);
+  });
+  it("A model class can update one record", done => {
+    assertName(User.findOneAndUpdate({ name: "Joe" }, { name: "Alex" }), done);
+  });
+  it("A model class can find a record with ID and update!", done => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+
+  it("A User can have their postcount incremented by 1.", done => {
+    User.update({ name: "Joe" }, { $inc: { postCount: 10 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(user => {
+        assert(user.postCount === 10);
+      });
     done();
   });
 });
