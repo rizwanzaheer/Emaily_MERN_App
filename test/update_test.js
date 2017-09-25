1e1a6acf54182da9601b756419e18c8350594aaf
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-25 21:08:05 +0500

    updating test through model instace

--- test/update_test.js
+++ test/update_test.js
@@ -7,18 +7,22 @@ describe("Updatting records", () => {
     joe = new User({ name: "Joe" });
     joe.save().then(() => done());
   });
+  function assertName(operation, done) {
+    operation.then(() => User.find({})).then(users => {
+      assert(users.length === 1);
+      assert(users[0].name === "Rizwan");
+      done();
+    });
+  }
   it("instance type using set n save", done => {
     console.log(joe);
     // set func is only change in memory not in db
     joe.set("name", "Rizwan");
-    joe
-      .save()
-      .then(() => User.find({}))
-      .then(users => {
-        assert(users.length === 1);
-        assert(users[0].name === "Rizwan");
-        done();
-      });
+    assertName(joe.save(), done);
     console.log(joe);
+  });
+  it("A model instance can update!", done => {
+    joe.update({ name: "Alex" });
+    done();
   });
 });
