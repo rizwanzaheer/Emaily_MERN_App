fd069229f24b37f5212cf1866de35e79bcc081ba
DESKTOP-8CJDCHV\Rizwan <rizwanzaheer003@gmail.com>
2017-09-26 10:14:19 +0500

    Custome validation added with test case

--- src/user.js
+++ src/user.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    validate: {
+      validator: name => name.length > 2,
+      message: "Name Must be longer then 2 characters."
+    },
+    required: [true, "Name is required!"]
+  },
   postCount: Number
 });
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
---
+++ test/validation_test.js
@@ -0,0 +1,29 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Validating records", () => {
+  it("requires a user name", done => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const {message} = validationResult.errors.name;
+    // console.log(validationResult);
+    assert(message === 'Name is required!');
+    done();
+  });
+  it("requires a user\'s name longer then 2 Characters!", done => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const {message} = validationResult.errors.name;
+    assert(message === 'Name Must be longer then 2 characters.');
+    done();
+  });
+
+  it('Disallows invalid records from being saved!' ,(done) => {
+    const user = new User({name : 'Al'});
+    user.save().catch((validationResult) =>{
+      const { message } = validationResult.errors.name;
+      assert(message === 'Name Must be longer then 2 characters.');
+      done();
+    });
+  });
+});
